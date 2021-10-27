73b39c087778d090d5f24d03b6cdd93d59ef6eb9
Kat <kyevsukov@gmail.com>
2021-10-27 12:35:09 -0400

    demo middleware

--- api/server.js
+++ api/server.js
@@ -6,7 +6,9 @@ const server = express();
 
 server.use(express.json());
 
-server.use('/api/hubs', hubsRouter);
+// server.use(customMiddleware); // custom middleware being used
+
+server.use('/api/hubs', customMiddleware, customMiddleware, hubsRouter);
 
 server.get('/', (req, res) => {
   res.send(`
@@ -15,9 +17,14 @@ server.get('/', (req, res) => {
   `);
 });
 
-server.use('*', (req, res, next) => {
+server.use('*', (req, res) => {
   // catch all 404 errors middleware
   res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` })
 });
 
 module.exports = server;
+
+function customMiddleware(req, res, next) { // custom middleware (declaring it)
+  console.log('web 47 rocks!')
+  next() // the req and res can proceed to the next middleware
+}
