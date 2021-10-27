88b73e88d73187c336eaa3161e42bde69f4c2fff
Kat <kyevsukov@gmail.com>
2021-10-27 13:32:20 -0400

    added to check id middleware adn updated router

--- api/hubs/hubs-middleware.js
+++ api/hubs/hubs-middleware.js
@@ -1,18 +1,28 @@
+
+const Hub = require('./hubs-model.js');
+
 function handleError(err, req, res, next) {
-    res.status(500 || err.status).json({
-      message: err.message,
-      prodMessage: 'something went really wrong!',
+  res.status(err.status || 500).json({
+    message: err.message,
+    prodMessage: 'something went really wrong!',
+  })
+}
+
+function checkHubId(req, res, next) {
+  Hub.findById(req.params.id)
+    .then(possibleHub => {
+      if (possibleHub) {
+        req.hub = possibleHub
+        next()
+      } else {
+        // res.status(404).json({ message: 'not found' })
+        next({ status: 404, message: 'not found!' })
+      }
     })
-  }
-  
-  function checkHubId(req, res, next) {
-    // hit the db with req.params.id
-    // check if something comes back
-    // if id is real, then next()
-    // if id not there, then next({ status: 404, mess })
-  }
-  
-  module.exports = {
-    handleError,
-    checkHubId,
-  }
+    .catch(next)
+}
+
+module.exports = {
+  handleError,
+  checkHubId,
+}
--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -15,15 +15,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', checkHubId, (req, res, next) => {
-  Hubs.findById(req.params.id)
-    .then(hub => {
-      if (hub) {
-        res.status(200).json(hub);
-      } else {
-        res.status(404).json({ message: 'Hub not found' });
-      }
-    })
-    .catch(next);
+  res.status(200).json(req.hub)
 });
 
 router.post('/', (req, res, next) => {
@@ -34,31 +26,23 @@ router.post('/', (req, res, next) => {
     .catch(next);
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkHubId, (req, res, next) => {
   Hubs.remove(req.params.id)
-    .then(count => {
-      if (count > 0) {
-        res.status(200).json({ message: 'The hub has been nuked' });
-      } else {
-        res.status(404).json({ message: 'The hub could not be found' });
-      }
+    .then(() => {
+      res.status(200).json({ message: 'The hub has been nuked' });
     })
     .catch(next);
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', checkHubId, (req, res, next) => {
   Hubs.update(req.params.id, req.body)
     .then(hub => {
-      if (hub) {
-        res.status(200).json(hub);
-      } else {
-        res.status(404).json({ message: 'The hub could not be found' });
-      }
+      res.status(200).json(hub);
     })
     .catch(next);
 });
 
-router.get('/:id/messages', (req, res, next) => {
+router.get('/:id/messages', checkHubId, (req, res, next) => {
   Hubs.findHubMessages(req.params.id)
     .then(messages => {
       res.status(200).json(messages);
@@ -66,7 +50,7 @@ router.get('/:id/messages', (req, res, next) => {
     .catch(next);
 });
 
-router.post('/:id/messages', (req, res, next) => {
+router.post('/:id/messages', checkHubId, (req, res, next) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
   Messages.add(messageInfo)
